28e3b592bc5d5c400c6a0efefbd76ef1e32768c5
wangrui <1634208912@qq.com>
2017-03-30 20:46:03 +0800

    one

--- js/index.js
+++ js/index.js
@@ -1,19 +1,9 @@
 $(document).ready(function(){
 
-	// 弹出层导航
-	$(".header .nav").click(function(){
-		$(".box").fadeIn(500);
-	});
-
-	$("#fixed").click(function(){
-		$(".box").fadeOut(500);
-	});
-
 	// 导航tab切换
 	$('.box ul li').mouseover(function(){
 		$('.box ul li').eq($(this).index()).addClass('on').siblings().removeClass('on');
 	});
-
 
 
 	// 轮播图
