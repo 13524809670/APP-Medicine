981ef2ff9b2969b704527868331401d970cb6d23
wangrui <1634208912@qq.com>
2017-03-30 12:44:40 +0800

    app

---
+++ js/index.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+	// 弹出层导航
+	$(".header .nav").click(function(){
+		$(".box").fadeIn(500);
+	});
+
+	$("#fixed").click(function(){
+		$(".box").fadeOut(500);
+	});
+
+	// 导航tab切换
+	$('.box ul li').mouseover(function(){
+		$('.box ul li').eq($(this).index()).addClass('on').siblings().removeClass('on');
+	});
+
+
+
+	// 轮播图
+	$(".banner_img li").eq(0).show();//显示第一张
+
+	var num = 0; //图片及底部按钮的 序列号
+	var timer;	 //定时器
+
+	var fn1 = function(){
+		// 第一张图片以3s的速度淡出，然后再出现下一张
+		$('.banner_img li').eq(num).stop().fadeOut(2000);
+		num++;
+
+		// 如果图片播放到第 3 张，那么就接着从第 1 张开始循环轮播
+		if(num > 2){num = 0;}
+
+		// 图片的第 num 张淡入
+		$('.banner_img li').eq(num).stop().fadeIn(2000);
+
+		// 按钮的颜色随着图片轮换
+		$('.banner_btn li').eq(num).addClass('cur').siblings('li').removeClass('cur');
+	}
+
+
+	var fn2 = function(){
+		$('.banner_img li').eq(num).stop().fadeOut(2000);
+
+		$(this).addClass('cur').siblings('li').removeClass('cur');
+
+		var i = $(this).index();
+
+		$('banner_btn li').eq(i).stop().fadeIn(2000);
+
+		num = i;
+	}
+
+	// 轮播时间间隔
+	timer = setInterval(fn1,2000);
+
+	// 当鼠标悬浮在banner时，取消定时器
+	$('.banner').hover(function(){
+		clearInterval(timer);
+	},function(){
+		//取消定时器 2s 后继续轮播 
+		clearInterval(timer);
+		timer = setInterval(fn1,2000)
+	});
+
+	// 点击按钮时，触发 fn2 的执行函数
+	$('.banner_btn li').click(fn2);
+})
